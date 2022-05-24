5a1d73c34d5f6b2d83a929da4b77669a534fa84d
KushagraSingal <104300046+KushagraSingal@users.noreply.github.com>
2022-05-24 13:59:20 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+SID = int(input('Enter SID: '))
+Enter SID: 21102039
+Name = str(input('Enter name: '))
+Enter name: Kushagra
+Gender = str(input('Enter Gender(F, M, or U for Unknown): '))
+Enter Gender(F, M, or U for Unknown): M
+Course_Name = str(input('Enter Course_Name: '))
+Enter Course_Name: ES1101
+CGPA = float(input('Enter CGPA: '))
+Enter CGPA: 9.8
+Student = [SID,Name,Gender,Course_Name,CGPA]
+print('Your data is:',Student)
+Your data is: [21102039, 'Kushagra', 'M', 'ES1101', 9.8]
